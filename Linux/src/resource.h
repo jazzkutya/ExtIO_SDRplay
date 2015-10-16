402779c6ff839b2963637bb76dba181d65c292f8
jazzkutya <jazzkutya@gmail.com>
2015-10-16 17:35:21 +0200

    Copied Windows dir to Linux for a starting point

---
+++ Linux/src/resource.h
@@ -0,0 +1,72 @@
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
+*/
+
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_SDRPLAY_SETTINGS                    114
+#define IDB_SDRPlay                             116
+#define IDB_SDRPlayR1                           118
+#define IDD_ADVANCED                            120
+#define IDC_GAINSLIDER                          1001
+#define IDC_PPM                                 1009
+#define IDC_ADVANCED_EXIT                       40000
+#define IDC_LOWARNING                           40001
+#define IDC_SETPOINT                            40002
+#define IDC_LO120                               40003
+#define IDC_ONESHOT                             40004
+#define IDC_ADVANCED_APPLY                      40005
+#define IDC_PPM_S                               40005
+#define IDC_TRACKINGPERIOD                      40006
+#define IDC_GR                                  40007
+#define IDC_POSTDCOFFSET                        40007
+#define IDC_TRACKTIME                           40008
+#define IDC_PERIODIC                            40009
+#define IDC_LO144                               40010
+#define IDC_CONTINUOUS                          40011
+#define IDC_LNAGRTHRES                          40012
+#define IDC_LOWARNING1                          40013
+#define IDC_LO168                               40014
+#define IDC_IFMODE                              40015
+#define IDC_REFRESH                             40016
+#define IDC_LOAUTO                              40017
+#define IDC_IFBW                                40018
+#define IDC_DCSTAT                              40019
+#define IDC_GR_S                                40020
+#define IDC_SETPOINT_S                          40021
+#define IDC_RSPAGC                              40022
+#define IDC_Advanced                            40023
+#define IDC_LNASLIDER                           40024
+#define IDC_LoadDefaults                        40025
+#define IDC_EXIT                                40026
+#define IDC_LNASTATE                            40027
+#define IDC_LNAGR                               40028
+#define IDC_IFGR                                40029
+#define IDC_MIXERGR                             40030
+#define IDC_MXRSTATE                            40031
+#define IDC_SAMPLERATE                          40032
